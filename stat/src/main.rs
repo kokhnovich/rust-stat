c4473a813d9a8a3e98437afba957100ddd04ba4e
kokhnovich <akokhnovich@yandex.ru>
2020-03-28 10:45:05 +0000

    compiled proto with prost

--- stat/src/main.rs
+++ stat/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+use std::fs;
+
+use fleetspeak::Packet;
+
+fn main() -> std::io::Result<()> {
+    fleetspeak::startup("0.0.1")?;
+
+    loop {
+        let packet = fleetspeak::collect(Duration::from_secs(1))?;
+
+        let request: String = packet.data;
+        // let attr = fs::metadata(request)?;
+        let response: String = format!("Hello {}!", request);
+
+        fleetspeak::send(Packet {
+            service: String::from("stat"),
+            kind: None,
+            data: response,
+        })?;
+    }
 }
