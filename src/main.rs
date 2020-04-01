8480e4e9d0104dfe0bfc6a35f1cd8cbb530e85f5
kokhnovich <akokhnovich@yandex.ru>
2020-04-01 15:57:07 +0100

    split into functions

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,37 @@ pub mod stat {
     include!(concat!(env!("OUT_DIR"), "/stat.rs"));
 }
 
+fn SendData(req:stat::Request, meta: fs::Metadata) -> std::io::Result<()> {
+    let resp = stat::Response {
+        path: req.path,
+        size: meta.len(),
+        mode: meta.st_mode() as u64,
+        extra: Some(stat::response::Extra {
+            blocks: meta.st_blocks(),
+            io_blocks: meta.st_blksize(),
+            inode: meta.st_ino(),
+            links: meta.st_nlink(),
+        }),
+    };
+    fleetspeak::send(Packet {
+        service: String::from("stat"),
+        kind: Some("response".to_string()),
+        data: resp,
+    })?;
+    Ok(())
+}
+
+fn SendError(e: std::io::Error) -> std::io::Result<()> {
+    fleetspeak::send(Packet {
+        service: String::from("stat"),
+        kind: Some("error".to_string()),
+        data: stat::Error {
+            what: e.to_string(),
+        },
+    })?;
+    Ok(())
+}
+
 fn main() -> std::io::Result<()> {
     fleetspeak::startup("0.1.1")?;
 
@@ -21,33 +52,8 @@ fn main() -> std::io::Result<()> {
         let req: stat::Request = packet.data;
 
         match fs::metadata(&req.path) {
-            Ok(meta) => {
-                let resp = stat::Response {
-                    path: req.path,
-                    size: meta.len(),
-                    mode: meta.st_mode() as u64,
-                    extra: Some(stat::response::Extra {
-                        blocks: meta.st_blocks(),
-                        io_blocks: meta.st_blksize(),
-                        inode: meta.st_ino(),
-                        links: meta.st_nlink(),
-                    }),
-                };
-                fleetspeak::send(Packet {
-                    service: String::from("stat"),
-                    kind: Some("response".to_string()),
-                    data: resp,
-                })?;
-            }
-            Err(e) => {
-                fleetspeak::send(Packet {
-                    service: String::from("stat"),
-                    kind: Some("error".to_string()),
-                    data: stat::Error {
-                        what: e.to_string(),
-                    },
-                })?;
-            }
-        }
+            Ok(meta) => SendData(req, meta),
+            Err(e) => SendError(e),
+        }?;
     }
 }
